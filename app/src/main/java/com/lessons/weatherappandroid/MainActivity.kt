9feb4c1792c95a3f9b2ee81734e06d413186c001
kusch ivan <i-kush@mail.ru>
2021-07-28 11:42:00 +0700

    init

---
+++ app/src/main/java/com/lessons/weatherappandroid/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.lessons.weatherappandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lessons.weatherappandroid.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
